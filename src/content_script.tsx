24e3d0063f8ed4df58570e604cda766625e54f01
jack <jackng9995@gmail.com>
2023-05-13 13:22:21 +0800

    example

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,3 +1,6 @@
+//@ts-ignore
+// import { Icon123 } from "@tabler/icons-react";
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.color) {
     console.log("Receive color = " + msg.color);
